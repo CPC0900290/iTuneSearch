a0f3d5ec8dffbce096253acdcd9ff3dfc7c70cf7
CPC0900290 <62605033+CPC0900290@users.noreply.github.com>
2024-07-10 15:27:31 +0800

    Increase readme and License

---
+++ iTuneSearch/iTuneSearch/View/SearchMusicViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SearchMusicViewController.swift
+//  iTuneSearch
+//
+//  Created by Pin Chen on 2024/7/10.
+//
+
+import UIKit
+
+class SearchMusicViewController: UIViewController {
+  @IBOutlet weak var collectionView: UICollectionView!
+  @IBOutlet weak var searchButton: UIButton!
+  @IBOutlet weak var searchTextField: UITextField!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ iTuneSearch/iTuneSearch/View/TrackCell/TrackCell.swift
@@ -0,0 +1,17 @@
+//
+//  TrackCell.swift
+//  iTuneSearch
+//
+//  Created by Pin Chen on 2024/7/10.
+//
+
+import Foundation
+import UIKit
+
+class TrackCell: UICollectionViewCell {
+  
+  @IBOutlet weak var longDescription: UILabel!
+  @IBOutlet weak var trackTimeMillis: UILabel!
+  @IBOutlet weak var trackName: UILabel!
+  @IBOutlet weak var trackImage: UIImageView!
+}
